98031b96ee62f527809b41df2c098b43ff949b99
Abdul Sami Shaikh <abdulsamishaikh232@gmail.com>
2026-08-11 21:20:57 +0530

    Fixed the Like Count bug

--- app/posts/PostCard.tsx
+++ app/posts/PostCard.tsx
@@ -2,21 +2,24 @@
 import { useState } from "react";
 
 export default function PostCard({ post }: { post: any }) {
-    const [liked, setLiked] = useState(false);
-    const [count, setCount] = useState(0);
+  const [likes, setLikes] = useState<Record<number, number>>({});
 
-    return(
-        <li className="border rounded px-2 py-4">
-            {post.id} <br />
-            {post.title} <br />
-            {post.body} <br />
-            <button
-                className="border rounded px-2 py-1"
-                onClick={() => setLiked(!liked)}
-        >
-            {liked ? "❤️" : "🤍"}
-        </button>
-        <button onClick={() => setCount(count + 1)}>{count}</button>
-        </li>
-    )
+  const handleLike = (id: number) => {
+    setLikes(prev => ({ ...prev, [id]: (prev[id] ?? 0) + 1}));
+  };
+  const [count, setCount] = useState(0);
+
+  return (
+    <li className="border rounded px-2 py-4">
+      {post.id} <br />
+      {post.title} <br />
+      {post.body} <br />
+      <button
+        className="border rounded px-2 py-1"
+        onClick={() => handleLike(post.id)}
+      >
+        ❤️ {likes[post.id] ?? 0}
+      </button>
+    </li>
+  );
 }
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,16 +1,15 @@
 import PostCard from "./PostCard";
 
 export default async function PostsPage() {
-    const res = await fetch("https://dummyjson.com/posts")
-    if (!res.ok) throw new Error("Failed to fetch posts")
-    const data = await res.json();
-    
-    
-    return(
-        <ul className="grid grid-cols-1 gap-6 m-3 md:grid-cols-2">
-            {data.posts.slice(0, 10).map((post: any) => (
-                <PostCard key={post.id} post={post} />
-            ))}
-        </ul>
-    )
+  const res = await fetch("https://dummyjson.com/posts");
+  if (!res.ok) throw new Error("Failed to fetch posts");
+  const data = await res.json();
+
+  return (
+    <ul className="grid grid-cols-1 gap-6 m-3 md:grid-cols-2">
+      {data.posts.slice(0, 10).map((post: any) => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </ul>
+  );
 }
