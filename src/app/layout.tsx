b919f5027c0267b768aca48eb35b9ea370cb1952
Abdul Sami Shaikh <abdulsamishaikh232@gmail.com>
2026-08-16 16:37:51 +0530

    fix: src folder issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/footer";
 
 export default function RootLayout({
   children,
